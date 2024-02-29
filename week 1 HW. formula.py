5eb8aee923d8398a1460310ee1b697d813e94682
Yizhen <161006740+yizhenzhenv@users.noreply.github.com>
2024-02-29 09:44:25 +0800

    Add files via upload
    一元二次公式解

---
+++ week 1 HW. formula.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 09:28:25 2024
+
+@author: student
+"""
+
+a=float(input ("a="))
+b=float(input ("b="))
+c=float(input ("c="))
+X1=(-b+(b**2-4*a*c)**(0.5))/(2*a)
+X2=(-b-(b**2-4*a*c)**(0.5))/(2*a)
+print("1st root is",X1)
+print("2nd root is",X2)
